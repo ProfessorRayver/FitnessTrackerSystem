256d2717c27c6d1c23f7426843833f1561daafa9
Rayver S. Reyes <121501115+ProfessorRayver@users.noreply.github.com>
2024-10-20 13:46:16 +0800

    Add files via upload

--- src/main/java/com/mycompany/fitnesstrackerdemo/FitnessTrackerDemo.java
+++ src/main/java/com/mycompany/fitnesstrackerdemo/FitnessTrackerDemo.java
@@ -11,6 +11,6 @@ package com.mycompany.fitnesstrackerdemo;
 public class FitnessTrackerDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("demo practice");
     }
 }
