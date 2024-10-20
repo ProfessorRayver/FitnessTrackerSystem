1489ab218416cd2cac7c507d8089116c7534803c
JM-COLADILLA <coladillajanmichael04@gmail.com>
2024-10-20 13:58:53 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/fitnesstrackerdsa/TrySomething.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fitnesstrackerdsa;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author colad
+ */
+public class TrySomething extends JFrame {
+    
+    TrySomething(){
+        setSize(400, 400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+    }
+    public static void main (String[] args){
+        new TrySomething();
+    }
+}
