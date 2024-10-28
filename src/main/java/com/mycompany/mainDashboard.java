8c82d965622802686d8e9ebca6a793cc36a59bb6
Rayver S. Reyes <121501115+ProfessorRayver@users.noreply.github.com>
2024-10-28 21:36:13 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/mainDashboard.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fitnesstrackerdemo;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+
+
+
+/**
+ *
+ * @author User
+ */
+public class mainDashboard extends JFrame {
+    // constructor
+    private JLabel titleDash, imgLabel;
+    private JButton btnWorkout, btnMeals, btnProgress, btnNotifications;
+    private JPanel pnButton;
+
+    mainDashboard() {
+
+        // frame
+        setSize(800, 800);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+       // for the picture
+        ImageIcon ImageIcon = new ImageIcon("\\C:\\Users\\colad\\Downloads\\PUP HealthTrackerBuddy.png\\"); //KINDLY CHANGE FILE PATH DIRECTORY
+        imgLabel = new JLabel(ImageIcon);
+        imgLabel.setBounds(0, 0, 800, 330);
+        add(imgLabel);
+        
+        // components
+        titleDash = new JLabel("Welcome! Fitness Tracker Dashboard", SwingConstants.CENTER);
+        titleDash.setBounds(0, 360, 800, 30);
+        titleDash.setFont(new Font("Tahoma", Font.BOLD, 20));
+        add(titleDash);
+     
+        // buttons
+        btnWorkout = new JButton("My Workout");
+       
+        btnMeals = new JButton("My Meals");
+        
+        btnProgress = new JButton("View Progress");
+        
+        btnNotifications = new JButton("Notifications");
+        
+        pnButton = new JPanel();
+        pnButton.setLayout(new GridLayout(1, 4, 50, 20));
+        pnButton.setBounds(90, 430, 600, 60);
+        pnButton.add(btnWorkout);
+        pnButton.add(btnMeals);
+        pnButton.add(btnProgress);
+        pnButton.add(btnNotifications);
+        
+        add(pnButton);
+        
+        setVisible(true);
+        
+        revalidate();
+        repaint();
+    }
+        
+           public static void main(String[] args) {
+        mainDashboard mainDashboard = new mainDashboard();
+        mainDashboard.setVisible(true);
+    }
+}
+    
+
+   
